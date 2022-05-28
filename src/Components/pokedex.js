71f80e3944b2e44c3df0e829b8b87b7bff0e0f2f
P-Colaco <pedrovitorc.pvc@gmail.com>
2022-05-28 19:20:26 -0300

    First commit

--- src/Components/pokedex.js
+++ src/Components/pokedex.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Pagination from "./Pagination";
 import Pokemon from "./Pokemon";
 
 const Pokedex = (props) => {
-    const { pokemons, loading } = props;
-    console.log("pokemons ", pokemons)
+    const { pokemons, loading, page, setPage, totalPages } = props;
+    const onLeftClickHandler = () => {
+        if(page > 0) {
+            setPage(page-1)
+        }
+    }
+    const onRightClickHandler = () => {
+        if(page+1 !== totalPages)
+            setPage(page+1)
+    }
     return (
         <div>
             <div className="pokedex-header">
                 <h1>Pokedex</h1>
-                <div>Paging:</div>
+                <Pagination
+                    page={page+1}
+                    totalPages={totalPages}
+                    onLeftClick={onLeftClickHandler}
+                    onRightClick={onRightClickHandler}
+                />
             </div>
             {loading ? (
                 <div>Loading, wait a min Pokemaster</div>
